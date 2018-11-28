cbfc189f5be6f9395cfbe20cecd999d2d9c490a1
FrostWolf1 <44281625+FrostWolf1@users.noreply.github.com>
2018-11-29 09:40:43 +1100

    Create PauseFunction.py

---
+++ Functions/PauseFunction.py
@@ -0,0 +1,5 @@
+#Python has a module called time, which I use here to define my own function 'pause'#
+import time
+#Here I define my function, the 'a' is a variable which I use to make it possible to add any delay you want.#
+def pause(a = 1):
+  time.sleep(a)
