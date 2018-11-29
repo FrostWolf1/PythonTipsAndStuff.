8a845c017757df7cf00c2b8f1231e4fef0cc9b08
FrostWolf1 <44281625+FrostWolf1@users.noreply.github.com>
2018-11-30 08:41:16 +1100

    Update and rename ChooseYourOwnAdventure to ChooseYourOwnAdventure.py

---
+++ Functions/ChooseYourOwnAdventure.py
@@ -0,0 +1,4 @@
+#Here I will show you a bunch of tips and functions for making Choose Your Own Adventure games.#
+#I will import system, a function that lets you mess with files.#
+import sys 
+#I will be defining a few functions for a cyoa (Choose You Own Adventure).#
